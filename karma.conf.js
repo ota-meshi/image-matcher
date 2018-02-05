ffab42fdaed35f221c8e9ae78e77da3fe66929ca
ota-meshi <otameshiyo23@gmail.com>
2018-02-05 19:57:20 +0900

    [add] test

---
+++ karma.conf.js
@@ -0,0 +1,125 @@
+// Karma configuration
+// Generated on Thu Jan 19 2017 10:39:28 GMT+0900 (東京 (標準時))
+'use strict';
+module.exports = function(config) {
+	config.set({
+
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '',
+
+
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['jasmine'],
+
+
+		// list of files / patterns to load in the browser
+		files: [
+			'dist/image-matcher.js',
+			'src/test/specs/*_spec.js',
+			'src/test/specs/**/*_spec.js',
+		],
+
+
+		// list of files to exclude
+		exclude: [
+		],
+
+
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {
+			'dist/*.js': ['coverage'],
+			'src/test/**/*.js': ['babel'],
+		},
+
+		//karma-babel-preprocessor
+		babelPreprocessor: {
+			options: {
+				presets: ['es2015'],
+				sourceMap: 'inline'
+			},
+			filename(file) {
+				return file.originalPath.replace(/\.js$/, '.es5.js');
+			},
+			sourceFileName(file) {
+				return file.originalPath;
+			}
+		},
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['html', 'mocha', 'coverage', 'junit', 'karma-remap-istanbul'],
+		htmlReporter: {
+			outputFile: 'report/units.html',
+			pageTitle: 'Unit Tests',
+			subPageTitle: 'A sample project description',
+			groupSuites: true,
+			useCompactStyle: true,
+			useLegacyStyle: true
+		},
+		// optionally, configure the reporter
+		coverageReporter: {
+			dir: 'coverage/dist',
+			reporters: [
+				{
+					type: 'html'
+				},
+				{ // jenkins xml
+					type: 'cobertura',
+					file: 'cobertura.xml'
+				},
+			]
+		},
+		remapIstanbulReporter: {
+			remapOptions: {}, //additional remap options
+			reportOptions: {}, //additional report options
+			reports: {
+				lcovonly: 'coverage/remap/lcov/lcov.info',
+				html: 'coverage/remap/html',
+				cobertura: 'coverage/remap/cobertura/cobertura.xml'
+			}
+		},
+		junitReporter: {
+			outputDir: 'report'
+		},
+
+
+		// web server port
+		port: 9876,
+
+
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
+
+
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_DEBUG,
+
+
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
+
+
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		// browsers: ['ChromeHeadless'],
+		// browsers: ['Chrome', 'IE_no_addons'],
+		browsers: ['Chrome'],
+		customLaunchers: {
+			'IE_no_addons': {
+				base: 'IE',
+				flags: ['-extoff']
+			},
+		},
+
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: false,
+
+		// Concurrency level
+		// how many browser should be started simultaneous
+		concurrency: Infinity
+	});
+};
---
+++ src/test/specs/jasmine_spec.js
@@ -0,0 +1,59 @@
+/*eslint-env es6*/
+/*eslint prefer-arrow-callback:"off", object-shorthand:"off"*/
+'use strict';
+window.testCode = function() {
+	function find(selector, html) {
+		const r = document.querySelector(selector);
+		if (!r) {
+			const doc = document.createElement('div');
+			doc.innerHTML = html;
+			const children = doc.children;
+			if (children[0]) {
+				const c = children[0];
+				document.body.appendChild(c);
+				return c;
+			}
+		}
+		return r;
+	}
+
+	describe('image-matcher jasmine example', function() {
+		function init(canvas) {
+			const ctx = canvas.getContext('2d');
+			ctx.fillStyle = '#fff';
+			ctx.fillRect(0, 0, canvas.width / 2, canvas.height / 2);
+
+			ctx.fillStyle = '#ffd';
+			ctx.fillRect(canvas.width / 2, 0, canvas.width / 2, canvas.height / 2);
+
+			ctx.fillStyle = '#fdf';
+			ctx.fillRect(0, canvas.height / 2, canvas.width / 2, canvas.height / 2);
+
+			ctx.fillStyle = '#dff';
+			ctx.fillRect(canvas.width / 2, canvas.height / 2, canvas.width / 2, canvas.height / 2);
+			return canvas;
+		}
+		const targetCanvas = init(find('#target', '<canvas width="100" height="100"></canvas>'));
+		const targetCtx = targetCanvas.getContext('2d');
+		targetCtx.fillStyle = '#fefefe';
+		targetCtx.font = '16px sans-serif';
+		targetCtx.fillText('test', 30, 30);
+		const answerCanvas = init(find('#answer', '<canvas width="100" height="100"></canvas>'));
+
+		it('expect(targetCanvas).toMatchImage(answerCanvas, {tolerance: 10, delta: \'5%\', blurLevel: 1})', function() {
+			expect(targetCanvas).toMatchImage(answerCanvas, {tolerance: 10, delta: '5%', blurLevel: 1, log: true});
+		});
+		it('expect(targetCanvas).toMatchImage(answerCanvas, {delta: \'10%\', blurLevel: 1})', function() {
+			expect(targetCanvas).toMatchImage(answerCanvas, {delta: '10%', blurLevel: 1, log: true});
+		});
+		it('expect(targetCanvas).not.toMatchImage(answerCanvas, {delta: \'5%\', blurLevel: 1})', function() {
+			expect(targetCanvas).not.toMatchImage(answerCanvas, {delta: '5%', blurLevel: 1, log: true});
+		});
+		it('expect(targetCanvas).not.toMatchImage(answerCanvas)', function() {
+			expect(targetCanvas).not.toMatchImage(answerCanvas, {log: true});
+		});
+
+	});
+
+};
+window.testCode();
